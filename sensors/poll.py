d2b3962d57e218b4ffcca213874f3ce19aac2ca2
vaibhav <vaibhavkumar049@gmail.com>
2022-03-21 23:40:40 +0530

    added

--- sensors/poll.py
+++ sensors/poll.py
@@ -19,6 +19,7 @@ class Poll(PollingSensor):
 
     def poll(self):
         data = requests.get("http://653e-43-252-251-77.ngrok.io/orders/pending")
+        data = data.json()
         if not 'res' in data:
             self._dispatch_trigger(data)
                     
